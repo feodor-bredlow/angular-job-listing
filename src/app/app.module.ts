b09c99e574464f259da1c1086856be8f5f526915
Feodor Tobler <feodor-tobler>
2021-11-30 23:27:22 +0100

    add list to project and include material components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,23 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import { MatCardModule } from "@angular/material/card";
+import { MatExpansionModule } from "@angular/material/expansion"
+
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { JobListComponent } from './jobs/job-list/job-list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    JobListComponent
   ],
   imports: [
     BrowserModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    MatCardModule,
+    MatExpansionModule
   ],
   providers: [],
   bootstrap: [AppComponent]
